db3f5d167b6340d10ef0401b3ed672c400e4b786
Ekaterina Tyurina <tiurina.kate@gmail.com>
2022-05-10 17:10:08 +0300

    Task001 Verification Square

---
+++ Task001_Verification_Square/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите первое число и нажмите клавишу Enter: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число и нажмите клавишу Enter: ");
+int b = int.Parse(Console.ReadLine());
+if(a == b * b)
+{
+    Console.WriteLine($"{a} является квадратом числа {b}");
+}
+else
+{
+    Console.WriteLine($"{a} НЕ является квадратом числа {b}");
+}
