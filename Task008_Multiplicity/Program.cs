7406095312ee29063694dbe80587114061934083
Ekaterina Tyurina <tiurina.kate@gmail.com>
2022-05-10 20:56:12 +0300

    Task008 Multyplicity

---
+++ Task008_Multiplicity/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите первое число и нажмите клавишу Enter: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число и нажмите клавишу Enter: ");
+int b = int.Parse(Console.ReadLine());
+int rem = 1;
+if(a % b == 0)
+{
+    Console.WriteLine($"{a} кратно {b}");
+}
+else
+{
+    rem = a % b;
+    Console.WriteLine($"{a} не кратно {b}. Остаток от деления {rem}");
+}
