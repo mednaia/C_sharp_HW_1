3f30a0debef61191f29475fe11c9ab2feffec0f6
Ekaterina Tyurina <tiurina.kate@gmail.com>
2022-05-10 18:10:00 +0300

    Task004 Parity 1 to N

---
+++ Task004_Parity_1_to_N/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число и нажмите клавишу Enter: ");
+int a = int.Parse(Console.ReadLine());
+int start = 1;
+int end = a;
+while(start<=end)
+{
+    if(start % 2 == 0)
+    {
+        Console.WriteLine(start);
+        start = start + 1;
+    }
+    else
+    {
+        start = start + 1;
+    }
+}
