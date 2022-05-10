be5bb05921baf03cf59aa9c97610ba93f8d56c5b
Ekaterina Tyurina <tiurina.kate@gmail.com>
2022-05-10 21:52:18 +0300

    Task006 Find the biggest number

---
+++ Task006_Find_biggest_number/Program.cs
@@ -0,0 +1,12 @@
+﻿int number = new Random().Next(10, 99);
+Console.WriteLine(number);
+int numberA = number / 10;
+int numberB = number % 10;
+Console.WriteLine($"Первая цифра числа: {numberA}");
+Console.WriteLine($"Вторая цифра числа: {numberB}");
+int max = numberA;
+int min = numberB;
+if(numberA > numberB)
+     Console.WriteLine($"Max: {numberA}  Min: {numberB}");
+else 
+     Console.WriteLine($"Max: {numberB}  Min: {numberA}");
