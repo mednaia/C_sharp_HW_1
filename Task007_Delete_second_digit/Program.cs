023d9f3072eba2599f9eb86e0881708eb4ce0c55
Ekaterina Tyurina <tiurina.kate@gmail.com>
2022-05-10 22:08:27 +0300

    Task007 Delete second digit

---
+++ Task007_Delete_second_digit/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число и нажмите клавишу Enter: ");
+int number = int.Parse(Console.ReadLine());
+if(number>99 && number<1000)
+{
+    int numberA = number / 100;
+    int numberC = number % 10;
+    int newNumber = (numberA*10) + numberC;
+    Console.WriteLine(newNumber);
+}
+else
+{
+    Console.WriteLine("Некорректное значение");
+}          
