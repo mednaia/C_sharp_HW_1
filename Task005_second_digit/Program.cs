806ec6df7c602164400213246f334b4b64968010
Ekaterina Tyurina <tiurina.kate@gmail.com>
2022-05-10 18:27:02 +0300

    Task005 Second digit

---
+++ Task005_second_digit/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Введите число и нажмите клавишу Enter: ");
+int number = int.Parse(Console.ReadLine());
+if(number>99 && number<1000)
+{
+    do
+    {
+        number = number / 10;
+        if(number < 100)
+            {
+                do
+                    {
+                        number = number - 10;
+                        if (number < 10)
+                        {
+                            Console.WriteLine($"Вторая цифра числа: {number}");
+                        }
+                    }
+                    while (number > 10);
+            }
+    }
+    while (number > 100);
+}
+else
+{
+    Console.WriteLine("Некорректное значение");
+}          
