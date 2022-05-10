6192408422a024caef3507a70bb6d2e8bc6c8077
Ekaterina Tyurina <tiurina.kate@gmail.com>
2022-05-10 17:34:42 +0300

    Task003 Parity

---
+++ Task003_Parity/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число и нажмите клавишу Enter: ");
+int a = int.Parse(Console.ReadLine());
+
+if(a % 2 == 0)
+{
+    Console.WriteLine($"{a} - четное число");
+}
+else
+{
+    Console.WriteLine($"{a} - нечетное число");
+}
