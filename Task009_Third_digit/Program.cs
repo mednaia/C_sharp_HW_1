38ebc76665de80398d0d0c5e5d6b1ee91acaf0ee
Ekaterina Tyurina <tiurina.kate@gmail.com>
2022-05-10 18:59:37 +0300

    Task009 Third digit

---
+++ Task009_Third_digit/Program.cs
@@ -0,0 +1,47 @@
+﻿Console.WriteLine("Введите число и нажмите клавишу Enter: ");
+int number = int.Parse(Console.ReadLine());
+if(number>99)
+{
+    do
+    {
+        number = number / 10;
+        if(number < 1000)
+            {
+                do
+                    {
+                        number = number - 10;
+                        if (number < 10)
+                        {
+                            Console.WriteLine($"Третья цифра числа: {number}");
+                        }
+                    }
+                    while (number > 10);
+            }
+    }
+    while (number > 1000);
+}
+if(number < - 99)
+{
+    do
+    {
+        number = number / 10;
+        if(number > - 1000)
+            {
+                do
+                    {
+                        number = number + 10;
+                        if (number > - 10)
+                        {
+                            number = number * -1;
+                            Console.WriteLine($"Третья цифра числа: {number}");
+                        }
+                    }
+                    while (number < - 10);
+            }
+    }
+    while (number < - 1000);
+}
+else
+{
+    Console.WriteLine("В данном числе нет третьей цифры");
+}          
