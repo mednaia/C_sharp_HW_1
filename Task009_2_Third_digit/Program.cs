322c361730f808322dec320fa70ae6b05e1be043
Ekaterina Tyurina <tiurina.kate@gmail.com>
2022-05-15 22:30:40 +0300

    task009 third digit from the end

---
+++ Task009_2_Third_digit/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите число и нажмите клавишу Enter: ");
+int number = int.Parse(Console.ReadLine());
+if(number < 100)
+    Console.WriteLine("В данном числе нет третьей цифры");
+int third = (number % 1000)/100;
+Console.WriteLine($"Третья цифра числа с конца {third}");
